f945f8c26730fb911b37fbe75e4c9238b9c3e86b
Nàdia Fischer <nadia.fischer@estudiantat.upc.edu>
2024-02-20 16:23:50 +0100

    Segona versió update_arrival

--- update_arrival.py
+++ update_arrival.py
@@ -1,16 +1,17 @@
 def update_arrival(h, m, d):
     """
-    >> > update_arrival(10, 45, 20)
+    >>> update_arrival(10, 45, 20)
     (11, 5)
-    >> > update_arrival(12, 30, 60)
+    >>> update_arrival(12, 30, 60)
     (13, 30)
-    >> > update_arrival(22, 0, 120)
+    >>> update_arrival(22, 0, 120)
     (0, 0)
-    >> > update_arrival(23, 57, 5 + 24 * 60)
+    >>> update_arrival(23, 57, 5 + 24 * 60)
     (0, 2)
     """
     mt = h * 60 + m + d
-    hf = mt // 60 % 24
+    hf = mt // 60
+    hf = hf % 24
     mf = mt % 60
     return hf, mf
 
